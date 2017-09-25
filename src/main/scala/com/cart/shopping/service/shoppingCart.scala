ab2a4e1440a6bae99d4586a54a2b8280c87ac4c0
Nandkishore <findnanda@googlemail.com>
2017-09-26 00:57:25 +0100

    Step 2 complete

--- src/main/scala/com/cart/shopping/service/shoppingCart.scala
+++ src/main/scala/com/cart/shopping/service/shoppingCart.scala
@@ -1,10 +1,21 @@
 package com.cart.shopping.service
 
-import com.cart.shopping.datamodel.ShoppingItem
+import com.cart.shopping.datamodel._
+
+import scala.collection.JavaConversions.mapAsJavaMap
 
 object shoppingCart {
 
   def calculatePayment(items: List[ShoppingItem]): BigDecimal = {
     items.map(item => item.cost).sum
+  }
+
+  def calculatePaymentWithServiceCharge(items: List[ShoppingItem]): BigDecimal = {
+
+    if (items.filter(_.isBeverage).size == items.size) calculatePayment(items) else calculatePayment(items) + withServiceCharge(items,calculatePayment(items));
+  }
+
+  def withServiceCharge(items: List[ShoppingItem], payment:BigDecimal): BigDecimal = {
+    return if (items.count(_.itemType.equals("hot")) >= 1)  payment * 0.20 else  payment * 0.10
   }
 }
